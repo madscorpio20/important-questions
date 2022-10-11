862d65a61db00abd2272a791f044d66583eea2a1
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2022-10-11 14:55:58 +0530

    Time: 14 ms (42.71%), Space: 13.6 MB (81.62%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-    int findMinCost(vector<int> &cost, vector<int> &dp, int curr)
+//     memoization
+    // int findMinCost(vector<int> &cost, vector<int> &dp, int curr)
+    // {
+    //     if(curr <0)
+    //         return 0;
+    //     if(dp[curr]!=-1)
+    //         return dp[curr];
+    //     if(curr == cost.size())
+    //         return dp[curr] = min(findMinCost(cost,dp,curr-1),findMinCost(cost,dp,curr-2));
+    //     return dp[curr] = min(findMinCost(cost,dp,curr-1),
+    //                         findMinCost(cost,dp,curr-2))+cost[curr];
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int s = cost.size();
+    //     vector<int> dp(s+1,-1);
+    //     return findMinCost(cost,dp,s);
+    // }
+    
+//     tabulation
+    int minCostClimbingStairs(vector<int>& cost)
     {
-        if(curr <0)
-            return 0;
-        if(dp[curr]!=-1)
-            return dp[curr];
-        if(curr == cost.size())
-            return dp[curr] = min(findMinCost(cost,dp,curr-1),findMinCost(cost,dp,curr-2));
-        return dp[curr] = min(findMinCost(cost,dp,curr-1),
-                            findMinCost(cost,dp,curr-2))+cost[curr];
-    }
-    int minCostClimbingStairs(vector<int>& cost) {
-        int s = cost.size();
-        vector<int> dp(s+1,-1);
-        return findMinCost(cost,dp,s);
+        int size = cost.size();
+        int prev1 = cost[0];
+        int prev2 = cost[1];
+        int curr;
+        for(int i=2; i<size; i++)
+        {
+            curr = min(prev1,prev2) + cost[i];
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return min(prev1,prev2);
     }
 };
