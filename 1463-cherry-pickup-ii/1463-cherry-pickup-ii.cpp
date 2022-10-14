3b203ae9a89a5d5537f9517e5787953093ce6365
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2022-10-14 14:54:00 +0530

    Time: 307 ms (27.23%), Space: 17 MB (48.74%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -32,6 +32,40 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        return find(grid,0,0,n-1,dp);
+        // return find(grid,0,0,n-1,dp);
+        for(int col1 = 0; col1<n; col1++)
+        {
+            for(int col2 = 0; col2<n; col2++)
+            {
+                if(col1 == col2)
+                    dp[m-1][col1][col2] = grid[m-1][col1];
+                else
+                    dp[m-1][col1][col2] = grid[m-1][col1] + grid[m-1][col2];
+            }
+        }
+        for(int row = m-2; row>=0; row--)
+        {
+            for(int col1 = 0; col1<n; col1++)
+            {
+                for(int col2 = 0; col2<n; col2++)
+                {
+                    if(col1 == col2)
+                        dp[row][col1][col2] = grid[row][col1];
+                    else
+                        dp[row][col1][col2] = grid[row][col1] + grid[row][col2];
+                    int maxi = -1e9;
+                    for(int i=-1; i<=1; i++)
+                    {
+                        for(int j=-1; j<=1; j++)
+                        {
+                            if(col1+i >=0 && col2+j >=0 && col1+i<n && col2+j<n)
+                                maxi = max(maxi,dp[row+1][col1+i][col2+j]);
+                        }
+                    }
+                    dp[row][col1][col2] += maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
