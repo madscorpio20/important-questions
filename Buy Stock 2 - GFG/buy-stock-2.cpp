2a16ac9cf7ebed09b347321c777a9dd6eb9ec683
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-01-15 10:43:13 +0530

    Added solution - LeetHub

---
+++ Buy Stock 2 - GFG/buy-stock-2.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&prices, int n) {
+        // Code here
+        long long profit = 0;
+        long long buy = 1e9;
+        for(auto it : prices)
+        {
+            buy = min(buy,it);
+            if(it > buy)
+            {
+                profit += it-buy;
+                buy = it;
+            }
+        }
+        return profit;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
