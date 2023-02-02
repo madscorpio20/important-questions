067c49c00a3af55ec89e8baa401d3e5856a097c4
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-02-03 00:30:01 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,25 +9,24 @@ using namespace std;
 
 class Solution{
 public:
-    int find(int arr[], int i, int j,vector<vector<int>> &dp)
+    int find(int i, int j, int arr[],vector<vector<int>> &dp)
     {
-        if(i==j)
+        if(i>=j)
         return 0;
-        if(dp[i][j]!=-1)
+        if(dp[i][j] != -1)
         return dp[i][j];
         int mini = 1e9;
         for(int k=i; k<j; k++)
         {
-            mini = min(mini, find(arr,i,k,dp) + find(arr,k+1,j,dp) + arr[i-1]*arr[j]*arr[k]);
+            mini = min(mini,find(i,k,arr,dp) + find(k+1,j,arr,dp) + arr[i-1]*arr[j]*arr[k]);
         }
-        // cout<<i<<" "<<j<<" "<<mini<<endl;
-        return dp[i][j] = mini;
+        return dp[i][j] =  mini;
     }
     int matrixMultiplication(int N, int arr[])
     {
         // code here
         vector<vector<int>> dp(N,vector<int> (N,-1));
-        return find(arr,1,N-1,dp);
+        return find(1,N-1,arr,dp);
     }
 };
 
