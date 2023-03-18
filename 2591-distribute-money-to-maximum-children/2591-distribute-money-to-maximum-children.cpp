0998814f33c3cd9b86011a904d84a71e5d5422bc
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-03-19 00:51:44 +0530

    Time: 4 ms (25.00%), Space: 5.9 MB (50.00%) - LeetHub

---
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if(money < children) return -1;
+        money -= children;
+        if(money%7 == 0)
+        {
+            if(money/7 > children) return children-1;
+            if(money/7 <= children) return money/7;
+            else
+                return money/7-2 > 0 ? money/7 -2 : 0;
+        }
+        if(money% 7 == 3)
+        {
+            if(money/7 == children-1)
+            {
+                return money/7-1 >=0 ? money/7-1 : 0;
+            }
+            if(money/7 > children) return children-1;
+            if(money/7 == children) return children-1;
+            if(money/7 < children) return money/7;
+        }
+        else
+        {
+            if(money/7 > children) return children -1;
+            if(money/7 == children)
+            {
+                return money/7 -1;
+            }
+            return money/7;
+        }
+        return money/7;
+    }
+};
