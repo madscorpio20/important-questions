03f37eb5e66da089db6f24d80f5059083f3bb405
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-04-11 12:21:21 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s1;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                s1.push(s[i]);
+            else
+            {
+                if(s1.empty())
+                    return false;
+                else if(s[i] == ')' && s1.top()!= '(')
+                    return false;
+                else if(s[i] == '}' && s1.top()!= '{')
+                    return false;
+                else if(s[i] == ']' && s1.top()!= '[')
+                    return false;
+                s1.pop();
+            }
+        }
+        return s1.size() == 0;
+    }
+};
