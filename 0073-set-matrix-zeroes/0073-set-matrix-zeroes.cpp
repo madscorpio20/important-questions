008a952945e288158230ae6f87279bc387cf9693
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-08-24 15:38:44 +0530

    Time: 12 ms (75.97%), Space: 13.3 MB (51.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,37 +3,46 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row(m,1);
-        vector<int> col(n,1);
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
-                    row[i] = 0;
-                    col[j] = 0;
+        int col = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j == 0){
+                        col = 0;
+                    }
+                    else
+                        matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=0; i<m; i++)
-        {
-            if(row[i] == 0)
-            {
-                for(int j=0; j<n; j++)
-                    matrix[i][j] = 0;
-            }
-        }
-        for(int j = 0; j<n; j++)
-        {
-            if(col[j] == 0)
-            {
-                for(int i=0; i<m; i++)
-                {
+        for(int i=1; i<m; i++){
+            if(matrix[i][0] == 0){
+                for(int j=0; j<n; j++){
                     matrix[i][j] = 0;
                 }
             }
         }
+        for(int j = 1; j<n; j++){
+            if(matrix[0][j] == 0){
+                for(int i=0; i<m; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(col == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
         return;
     }
 };
