1680577613fc65da4b1a8208834c829a88bde113
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-08-26 16:24:52 +0530

    Time: 165 ms (65.79%), Space: 22.8 MB (55.03%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    int helper(vector<vector<int>> &pairs,int ind, int prev, vector<vector<int>> &dp){
-        if(ind == pairs.size())
-            return 0;
-        if(dp[ind][prev+1]!=-1)
-            return dp[ind][prev+1];
-        int notPick = helper(pairs,ind+1,prev,dp);
-        int pick = 0;
+//     int helper(vector<vector<int>> &pairs,int ind, int prev, vector<vector<int>> &dp){
+//         if(ind == pairs.size())
+//             return 0;
+//         if(dp[ind][prev+1]!=-1)
+//             return dp[ind][prev+1];
+//         int notPick = helper(pairs,ind+1,prev,dp);
+//         int pick = 0;
         
-        if(prev == -1 || pairs[ind][0] > pairs[prev][1]){
-            pick = helper(pairs,ind+1,ind,dp) + 1;
-        }
-        return dp[ind][prev+1] = max(pick,notPick);
-    }
+//         if(prev == -1 || pairs[ind][0] > pairs[prev][1]){
+//             pick = helper(pairs,ind+1,ind,dp) + 1;
+//         }
+//         return dp[ind][prev+1] = max(pick,notPick);
+//     }
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(),pairs.end());
-        vector<vector<int>> dp(pairs.size(),vector<int>(pairs.size()+1,-1));
-        return helper(pairs,0,-1,dp);
+        // vector<vector<int>> dp(pairs.size(),vector<int>(pairs.size()+1,0));
+        // return helper(pairs,0,-1,dp);
+        int n = pairs.size();
+        vector<int> dp(n);
+        for(int i=0; i<n; i++){
+            int prev = 0;
+            for(int j=0; j<i; j++){
+                if(pairs[j][1] < pairs[i][0])
+                {
+                    prev = dp[j];
+                }
+            }
+            dp[i] = prev + 1;
+        }
+        int maxi = 0;
+        for(int i=0; i<n; i++){
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
     }
 };
