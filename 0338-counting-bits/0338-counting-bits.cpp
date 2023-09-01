42e8472d2449e788e0af5b2596313c641a463223
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-01 13:22:09 +0530

    Time: 3 ms (89.82%), Space: 7.9 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,15 +4,7 @@ public:
         vector<int> ans(n+1);
         ans[0] = 0;
         for(int i=1; i<=n; i++){
-            int cnt = 0;
-            int num = i;
-            while(num> 0){
-                if(num%2 == 1){
-                    cnt++;
-                }
-                num =num/2;
-            }
-            ans[i] = cnt;
+            ans[i] = ans[i/2] + i%2;        
         }
         return ans;
     }
