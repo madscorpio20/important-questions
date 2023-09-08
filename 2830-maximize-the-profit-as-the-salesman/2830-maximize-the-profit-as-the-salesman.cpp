9575469f109ca3bea77be91a91e1e9a7b61ad78c
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-08 17:55:07 +0530

    Time: 344 ms (97.54%), Space: 93.7 MB (99.47%) - LeetHub

--- 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    int find(vector<vector<int>> &offers, int ind){
-        int target = offers[ind][1] + 1;
-        int s = ind+1;
-        int ans = -1;
-        int e = offers.size()-1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(offers[mid][0] >= target){
-                e = mid-1;
-                ans = mid;
-            }else{
-                s = mid+1;
-            }
-        }
-        
-        return ans;
-    }
-
-    int helper(vector<vector<int>> &offers, int ind,vector<int> &dp){
-        if(ind >= offers.size())
-            return 0;
-        if(dp[ind]!=-1)
-            return dp[ind];
-        int notTake = helper(offers, ind+1,dp);
-        int take = 0;
-
-        int nextInd = find(offers,ind);
-        if(nextInd!=-1)
-        {
-            take = helper(offers,nextInd,dp) + offers[ind][2];
-        }else{
-            take = offers[ind][2];
-        }
-        return dp[ind] = max(take,notTake);
-    }
     int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        int maxi = 0;
         sort(offers.begin(),offers.end());
-        int ind = 0;
-        vector<int> dp(offers.size(),-1);
-        return helper(offers, ind,dp);
+        int siz = offers.size();
+        vector<int> dp(siz);
+        for(int i=siz-1; i>=0; i--){
+            int s = i+1;
+            int e = siz-1;
+            int ans = -1;
+            int target = offers[i][1];
+            while(s<=e){
+                int mid = (s+e)/2;
+                if(offers[mid][0] > target){
+                    e = mid-1;
+                    ans = mid;
+                }else{
+                    s = mid+1;
+                }
+            }
+            if(ans!=-1){
+                dp[i] = offers[i][2] + dp[ans];
+            }else{
+                dp[i] = offers[i][2];
+            }
+            maxi = max(maxi,dp[i]);
+            dp[i] = maxi;
+        }
+        return maxi;
     }
 };
