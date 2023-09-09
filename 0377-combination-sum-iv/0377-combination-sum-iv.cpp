4668dd31f3fb657cad340d3d0e810c10bda8d8aa
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-09 18:09:52 +0530

    Time: 3 ms (50.37%), Space: 6.7 MB (20.91%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -15,7 +15,15 @@ public:
         return dp[target] = ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
-        return helper(nums,0,target,dp);
+        vector<unsigned long long> dp(target+1,0);
+        // return helper(nums,0,target,dp);
+        dp[0] = 1;
+        for(int i=1; i<=target; i++){
+            for(int j=0; j<nums.size(); j++){
+                if(i - nums[j] >= 0 )
+                    dp[i] += dp[i-nums[j]];
+            }
+        }
+        return (int)dp[target];
     }
 };
