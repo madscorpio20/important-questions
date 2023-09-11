6a8a8b0ae2a7693a154ecf2b9a50d147bb7c362b
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-11 14:46:13 +0530

    Time: 10 ms (64.93%), Space: 12.6 MB (88.22%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        vector<pair<int,int>> v;
+        unordered_map<int,vector<int>> m;
+        vector<vector<int>> result;
         for(int i=0; i<groupSizes.size(); i++){
-            v.push_back({groupSizes[i],i});
-        }
-        sort(v.begin(),v.end());
-        
-        int ind = 0;
-        vector<vector<int>> ans;
-        while(ind <v.size()){
-            vector<int> curr;
-            int capacity = v[ind].first;
-            while(curr.size() < capacity){
-                curr.push_back(v[ind].second);
-                ind++;
+            int size = groupSizes[i];
+            m[size].push_back(i);
+            if(m[size].size() == size ){
+                result.push_back(m[size]);
+                m[size].clear();
             }
-            ans.push_back(curr);
         }
-        return ans;
+        return result;
     }
 };
