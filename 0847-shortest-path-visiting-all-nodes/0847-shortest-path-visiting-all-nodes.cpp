39d700aa6436d1495850c608780b26034795e9fc
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-17 17:54:57 +0530

    Time: 123 ms (23.41%), Space: 18.5 MB (25.10%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int target = 0;
+        int n = graph.size();
+        for(int i = 0; i<n; i++){
+            target = (target | 1<<i);
+        }
+        queue<pair<int,pair<int,int>>> q;
+        set<pair<int,int>>vis;
+        for(int i=0; i<n; i++){
+            q.push({i,{0,1<<i}});
+            vis.insert({i,1<<i});
+        }
+        int mini = 1e9;
+        while(!q.empty()){
+            int node = q.front().first;
+            int dis = q.front().second.first;
+            int mask = q.front().second.second;
+            q.pop();
+            if(mask == target){
+                mini = min(mini,dis);
+                continue;
+            }
+            for(int i=0; i<graph[node].size(); i++){
+                int val = graph[node][i];
+                if(!vis.count({val,mask | 1<<val})){
+                    vis.insert({val,mask| 1<<val});
+                    q.push({val,{dis+1,mask | 1<<val}});
+                }
+            }
+        }
+        return mini;
+    }
+};
