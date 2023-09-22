ff8bf5bfdc755eab08584dce4faa0f5568e03129
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-09-22 14:01:03 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (10.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(t.size() < s.size()) return false;
+        int m = s.size();
+        int n = t.size();
+        if(n < m) return false;
         int i1 = 0;
         int i2 = 0;
-        while(i2 < t.size()){
+        while(i2 < n && i1 < m){
             if(s[i1] == t[i2]){
                 i1++;
                 i2++;
@@ -12,7 +14,7 @@ public:
                 i2++;
             }
         }
-        if(i1 == s.size()) return true;
+        if(i1 == m) return true;
         return false;
     }
 };
