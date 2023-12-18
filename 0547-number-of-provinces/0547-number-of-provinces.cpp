b4879665ae431858c32f6b3968ab156e41e8ce4e
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2023-12-18 23:37:58 +0530

    Time: 20 ms (74.58%), Space: 16.9 MB (37.08%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>> &isConnected, vector<int> &vis, int curr)
-    {
-        queue<int> q;
-        q.push(curr);
-        vis[curr] = 1;
-        while(!q.empty())
-        {
-            int size = q.size();
-            for(int i=0; i<size; i++)
-            {
-                int front = q.front();
-                q.pop();
-                for(int j=0; j<isConnected.size(); j++)
-                {
-                    if(vis[j] == 0 && isConnected[front][j] == 1)
-                    {
-                        q.push(j);
-                        vis[j] = 1;
-                    }
-                }
+    void dfs(int node, vector<int> &vis, vector<vector<int>> &adj){
+        vis[node] = 1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfs(it, vis, adj);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<int> vis(isConnected.size()+1,0);
+        int n = isConnected.size();
+        vector<vector<int>> adj(n + 1);
+        for(int i=0; i<isConnected.size(); i++){
+            for(int j=0; j<isConnected[i].size(); j++){
+                if(isConnected[i][j] == 1){
+                    adj[i+1].push_back(j+1);
+                }
+            }
+        }
+        vector<int> vis(n +1, 0);
         int cnt = 0;
-        for(int i=0; i<isConnected.size(); i++)
-        {
-            if(vis[i] == 0)
-            {
+        // for(int i=0; i<adj.size(); i++){
+        //     for(int j=0; j<adj[i].size(); j++){
+        //         cout<<adj[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        for(int i=1; i<= n; i++){
+            if(!vis[i]){
+                // cout<<i<<endl;
                 cnt++;
-                bfs(isConnected,vis, i);
+                dfs(i, vis, adj);
             }
         }
         return cnt;
