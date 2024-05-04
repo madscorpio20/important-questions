3e283f61ca64baae96c67a733cba47f9a6778bc6
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2024-05-04 15:38:28 +0530

    Time: 1194 ms (11.28%), Space: 163.3 MB (5.01%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<string> q;
-        int cnt = 0;
-        unordered_map<string,int> m;
-        unordered_map<string,int> availableWords;
-        for(auto it: wordList)
-        {
-            availableWords[it]++;
+        map<string,int> mp;
+        for(auto it: wordList){
+            mp[it]++;
         }
-        m[beginWord]++;
+        map<string,int> vis;
+        vis[beginWord]++;
+        int cnt = 0;
+        queue<string> q;
         q.push(beginWord);
-        while(!q.empty())
-        {
+        while(!q.empty()){
             cnt++;
-            int n = q.size();
-            for(int k=0; k<n; k++)
-            {
+            int sz = q.size();
+            for(int i=0; i<sz; i++){
                 string curr = q.front();
                 q.pop();
-                if(curr == endWord) return cnt;
-                for(int i=0; i<curr.size(); i++)
-                {
-                    for(int j=0; j<26; j++)
-                    {
-                        string newWord = curr;
-                        newWord[i] = j + 'a';
-                        if(m[newWord]) continue;
-                        if(availableWords[newWord])
-                        {
-                            q.push(newWord);
-                            m[newWord]++;
+                for(int j=0; j<curr.size(); j++){
+                    string nxt = curr;
+                    for(int k=0; k<26; k++){
+                        nxt[j] = k + 'a';
+                        if( mp[nxt] && !vis[nxt]){
+                            vis[nxt] = 1;
+                            if(nxt == endWord) return cnt+1;
+                            q.push(nxt);
                         }
                     }
                 }
             }
-            
         }
         return 0;
         
