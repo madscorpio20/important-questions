8dd2290cea476f4ce82279c98c707580bbab9420
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2024-05-09 13:35:18 +0530

    Time: 171 ms (35.72%), Space: 107.1 MB (89.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        long long ans = 0;
-        sort(happiness.begin(), happiness.end());
-        reverse(happiness.begin(), happiness.end());
-        long long counter = 0;
-        for(int i=0; i<k; i++){
-            ans += (long long)happiness[i] - counter > 0 ? (long long)happiness[i] - counter: 0;
-            counter++;
+        long long cnt = 0;
+        sort(happiness.begin(),happiness.end());
+        int n = happiness.size();
+        long long j = 0;
+        for(int i=n-1; i>=0; i--){
+            long long curr = happiness[i];
+            if(curr > j)
+            cnt += (curr - j);
+            else break;
+            if(j+1 <k)j++;
+            else break;
         }
-        return ans;
+        return cnt;
     }
 };
