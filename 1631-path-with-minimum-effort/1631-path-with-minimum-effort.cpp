d9a0b72d2f342e13ce195275a86154a0ccb31fbd
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2024-05-26 12:54:52 +0530

    Time: 94 ms (53.81%), Space: 23.7 MB (40.88%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,30 +3,30 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        vector<vector<int>> dis(m, vector<int> (n, 1e9));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0,0,0});
-        dis[0][0] = 0;
-        vector<int> r = {-1,+1,0,0};
-        vector<int> c = {0,0,-1,+1};
+        vector<vector<int>> effort(m, vector<int> (n,1e9));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,                                 greater<pair<int,pair<int,int>>>> pq;
+        vector<int> d1 = {+1,-1,0,0};
+        vector<int> d2 = {0,0,-1,+1};
+        pq.push({0,{0,0}});
+        effort[0][0] = 0;
         while(!pq.empty()){
             auto it = pq.top();
-            int hi = it[0];
-            int row = it[1];
-            int col = it[2];
+            int dis = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
             pq.pop();
             for(int i=0; i<4; i++){
-                int adjRow = row + r[i];
-                int adjCol = col + c[i];
-                if(adjRow >= 0 && adjCol >=0 && adjRow < m && adjCol < n){
-                    int diff = max(hi,abs(heights[row][col] - heights[adjRow][adjCol]));
-                    if(diff < dis[adjRow][adjCol]){
-                        dis[adjRow][adjCol] = diff;
-                        pq.push({diff,adjRow,adjCol});
+                int newr = r + d1[i];
+                int newc = c + d2[i];
+                if(newr < m && newr>= 0 && newc < n && newc >=0){
+                    int currEffort = max(dis,abs(heights[r][c] - heights[newr][newc]));
+                    if(currEffort < effort[newr][newc]){
+                        effort[newr][newc] = currEffort;
+                        pq.push({currEffort,{newr,newc}});
                     }
                 }
             }
         }
-        return dis[m-1][n-1];
+        return effort[m-1][n-1];
     }
 };
