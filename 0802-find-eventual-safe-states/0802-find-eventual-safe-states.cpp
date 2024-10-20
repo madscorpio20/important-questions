a7190f55b39af331281eac32655ec87102913dd0
madscorpio20 <88392319+madscorpio20@users.noreply.github.com>
2024-10-20 23:43:48 +0530

    Time: 11 ms (99.87%), Space: 52.2 MB (71.75%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>> &graph, vector<int> & vis, vector<int> &res, vector<int> &pathVis){
+    bool dfs(int node, vector<vector<int>> &graph, vector<int> &safeNodes, vector<int> &vis,vector<int> &pathVis){
+        if(pathVis[node]) return false;
+        if(vis[node]) return safeNodes[node];
+        bool safe = true;
         vis[node] = 1;
+        pathVis[node] = 1;
         for(auto it: graph[node]){
-            if(!pathVis[it] && vis[it]) continue;
-            if(pathVis[it]){
-                return true;
-            }
-            pathVis[it] = 1;
-            bool flag = dfs(it, graph, vis, res, pathVis);
-            if(flag) return true;
-            pathVis[it] = 0;
-         }
-        return false;
+             bool curr_path_safe = dfs(it, graph, safeNodes, vis,pathVis);
+            curr_path_safe == false ? safe = false : safe = safe;
+        }
+        pathVis[node] = 0;
+        return safeNodes[node] = safe;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, 0);
-        vector<int> res;
-        vector<int> pathVis(n, 0);
+        vector<int> safeNodes(n, 1);
+        vector<int> vis(n,0);
+        vector<int> pathVis(n,0);
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                pathVis[i] = 1;
-                bool flag = dfs(i, graph, vis, res, pathVis);
-                if(!flag)
-                pathVis[i] = 0;
+                dfs(i, graph, safeNodes, vis, pathVis);   
             }
         }
+        vector<int> res;
         for(int i=0; i<n; i++){
-            if(pathVis[i] == 0){
-                res.push_back(i);
-            }
+            if(safeNodes[i]) res.push_back(i);
         }
         return res;
+        
     }
 };
